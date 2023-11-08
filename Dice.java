3d245551be49ff642102f1e55ffa9e2409a81f8c
Asus Vivobook <piyushdeshmukhr@gmail.com>
2023-11-08 17:08:08 +0000

    Added Player and Dice

--- Dice.java
+++ Dice.java
@@ -1,2 +1,13 @@
-public class Dice {
+import java.util.Random;
+
+public class Dice
+{
+    static int value;
+
+    public static int roll()
+    {
+        Random rand = new Random();
+        value = rand.nextInt(6) + 1;
+        return value;
+    }
 }
